7cd83c5cb1d4b3f190b92c3051974ce7a219ba09
Jefferson Silva <jefferson.font@gmail.com>
2023-10-19 11:11:55 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+        <h1>Hotsite de Venda de Carro Esportivo</h1>
+  </header>
+
+</template>
+
+<style scoped>
+
+header {
+  background-color: #ff4500;
+  color: #fff;
+  text-align: center;
+  padding: 10px 0;
+}
+
+h1 {
+    margin-top: 0;
+}
+</style>
